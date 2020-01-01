dc8aea700db163ea5507390c268e404a261b5a37
Lula Leus <lula@startmonday.com>
2020-01-01 18:43:36 +0000

    README added

---
+++ README.md
@@ -0,0 +1,13 @@
+#Cocktails club
+##Running
+Make sure that you have Golang installed. Then, in the project root directory, run
+``$ go run .``
+It is a shortcut for building and running application in one step. 
+
+##Linting
+Run the following command, to lint all the files in all subdirectories.
+``$ golint ./...``
+
+##Testing
+Run the following command, to run all tests.
+``$ go test ./... -cover ``
--- middleware/middleware.go
+++ middleware/middleware.go
@@ -11,7 +11,7 @@ func QueryChecker() gin.HandlerFunc {
 		query := c.Request.URL.Query()
 		if len(query) <= 0 {
 			c.Header(common.ErrorHeaderName, "Query parameter is required")
-			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{common.ErrorMessageKey: "Expect ingredient as query search parameter"})
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{common.ErrorMessageKey: "Expect ingredient as search query parameter"})
 		}
 	}
 }
